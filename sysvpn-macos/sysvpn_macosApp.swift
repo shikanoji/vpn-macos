8a1ba156351d8791001567b65b2bcadb39585537
Nguyen Dinh Thach <nguyen.dinh.thach@linecorp.com>
2022-08-29 18:39:06 +0700

    Initial Commit

---
+++ sysvpn-macos/sysvpn_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sysvpn_macosApp.swift
+//  sysvpn-macos
+//
+//  Created by Nguyen Dinh Thach on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct sysvpn_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sysvpn-macosUITests/sysvpn_macosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sysvpn_macosUITestsLaunchTests.swift
+//  sysvpn-macosUITests
+//
+//  Created by Nguyen Dinh Thach on 29/08/2022.
+//
+
+import XCTest
+
+class sysvpn_macosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
