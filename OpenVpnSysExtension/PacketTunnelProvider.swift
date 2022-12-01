2a28e1f5f7de98d5e2266efdfeb34e49eb8f5ad7
thucdv <thucdv@techlab.ai>
2022-12-01 15:15:30 +0700

    update vpn connection

--- OpenVpnSysExtension/PacketTunnelProvider.swift
+++ OpenVpnSysExtension/PacketTunnelProvider.swift
@@ -8,56 +8,110 @@ import NetworkExtension
 import os.log
 import TunnelKitManager
 import TunnelKitOpenVPNAppExtension
+import Foundation
+
 enum VPNProtocol {
     case openVPN
     case wireGuard
 }
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
-    let userDefaults = UserDefaults(suiteName: CoreAppConstants.AppGroups.main)
-
-    var type: VPNProtocol {
-        return getProtocol()
-    }
-
+ 
+    var lastConnectProtoAdapter: ProtoAdapter?
+    
     private lazy var openVPNAdapter = OpenVPNTunnelAdapter(packetTunnelProvider: self)
     private lazy var wireguardAdapter = WireGuardTunnelAdapter(packetTunnelProvider: self)
      
     override func startTunnel(options: [String: NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
-        switch type {
-        case .openVPN:
-            return openVPNAdapter.startTunnel(options: options, completionHandler: completionHandler)
-        case .wireGuard:
-            return wireguardAdapter.startTunnel(options: options, completionHandler: completionHandler)
+        
+        
+        var tunnelProtocol: VPNProtocol = .wireGuard
+        
+        if let tunnel = self.protocolConfiguration as? NETunnelProviderProtocol {
+            let protocolConfiguration = tunnel.providerConfiguration?[CoreAppConstants.VPNProtocolName.configurationField] as? String
+            if protocolConfiguration == CoreAppConstants.VPNProtocolName.openVpn {
+                tunnelProtocol = .openVPN
+            }
         }
+         
+        
+        os_log("%{public}s", log: OSLog(subsystem: "SysVPNIPC-OP", category: "IPC"), type: .default, "loadata \(tunnelProtocol)")
+        
+        lastConnectProtoAdapter = getTunnelByProtocol(proto: tunnelProtocol)
+        lastConnectProtoAdapter?.startTunnel(options: options, completionHandler: completionHandler)
     }
     
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        switch type {
-        case .openVPN:
-            return openVPNAdapter.stopTunnel(with: reason, completionHandler: completionHandler)
-        case .wireGuard:
-            return wireguardAdapter.stopTunnel(with: reason, completionHandler: completionHandler)
-        }
+        lastConnectProtoAdapter?.stopTunnel(with: reason, completionHandler: completionHandler)
     }
     
     override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)? = nil) {
-        switch type {
+        lastConnectProtoAdapter?.handleAppMessage(messageData, completionHandler: completionHandler)
+    }
+    
+    func getTunnelByProtocol(proto: VPNProtocol) -> ProtoAdapter {
+        switch proto {
         case .openVPN:
-            return super.handleAppMessage(messageData, completionHandler: completionHandler)
+            return openVPNAdapter
         case .wireGuard:
-            return wireguardAdapter.handleAppMessage(messageData, completionHandler: completionHandler)
+            return wireguardAdapter
         }
     }
     
-    func getProtocol() -> VPNProtocol {
-        let vpnProtocol = userDefaults?.string(forKey: "vpnProtocol") ?? "openVPN"
-         
-        os_log("%{public}s", log: OSLog(subsystem: "SysVPNIPC-OP", category: "IPC"), type: .default, "loadata \(vpnProtocol)")
+    public var interfaceName: String? {
+        guard let tunnelFileDescriptor = self.tunnelFileDescriptor else { return nil }
 
-        if vpnProtocol == "wireGuard" {
-            return .wireGuard
+        var buffer = [UInt8](repeating: 0, count: Int(IFNAMSIZ))
+
+        return buffer.withUnsafeMutableBufferPointer { mutableBufferPointer in
+            guard let baseAddress = mutableBufferPointer.baseAddress else { return nil }
+
+            var ifnameSize = socklen_t(IFNAMSIZ)
+            let result = getsockopt(
+                tunnelFileDescriptor,
+                2  ,
+                2 ,
+                baseAddress,
+                &ifnameSize)
+
+            if result == 0 {
+                return String(cString: baseAddress)
+            } else {
+                return nil
+            }
         }
-        return .openVPN
     }
+    
+    private var tunnelFileDescriptor: Int32? {
+        var ctlInfo = ctl_info()
+        withUnsafeMutablePointer(to: &ctlInfo.ctl_name) {
+            $0.withMemoryRebound(to: CChar.self, capacity: MemoryLayout.size(ofValue: $0.pointee)) {
+                _ = strcpy($0, "com.apple.net.utun_control")
+            }
+        }
+        for fd: Int32 in 0...1024 {
+            var addr = sockaddr_ctl()
+            var ret: Int32 = -1
+            var len = socklen_t(MemoryLayout.size(ofValue: addr))
+            withUnsafeMutablePointer(to: &addr) {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                    ret = getpeername(fd, $0, &len)
+                }
+            }
+            if ret != 0 || addr.sc_family != AF_SYSTEM {
+                continue
+            }
+            if ctlInfo.ctl_id == 0 {
+                ret = ioctl(fd, CTLIOCGINFO, &ctlInfo)
+                if ret != 0 {
+                    continue
+                }
+            }
+            if addr.sc_id == ctlInfo.ctl_id {
+                return fd
+            }
+        }
+        return nil
+    }
+     
 }
--- OpenVpnSysExtension/open_vpn_sys_extension_birdge_header.h
+++ OpenVpnSysExtension/open_vpn_sys_extension_birdge_header.h
@@ -8,4 +8,20 @@
 #ifndef open_vpn_sys_extension_birdge_header_h
 #define open_vpn_sys_extension_birdge_header_h
 #include "../sysvpn-macos/VPNCore/IPC/AuditTokenGetter.h"
+
+#define CTLIOCGINFO 0xc0644e03UL
+struct ctl_info {
+    u_int32_t   ctl_id;
+    char        ctl_name[96];
+};
+
+struct sockaddr_ctl {
+    u_char      sc_len;
+    u_char      sc_family;
+    u_int16_t   ss_sysaddr;
+    u_int32_t   sc_id;
+    u_int32_t   sc_unit;
+    u_int32_t   sc_reserved[5];
+};
+
 #endif /* open_vpn_sys_extension_birdge_header_h */
